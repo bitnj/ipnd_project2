eb16386a20d9d5a90027e504f5fc5cd8542164dd
nseas <neilseas@gmail.com>
2016-09-21 16:02:18 -0400

    First commit with reading of quiz and answer from files

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,80 +1,103 @@
 # IPND Stage 2 Final Project
 
-# Fill in the blank quiz using code adapted from mad_libs.py.  The user will be
-# be presented with a paragraph containing blank spaces that need to be filled
-# in with the correct answer.  Five attempts will be given to get each blank
-# correct.  If the user gets five incorrect answers in a row the quiz must be
-# restarted.  The blanks are labeled 1 through n, where blanks with the same
-# number are filled in with the same answer.  Answers are NOT case sensitive.
-# If a correct answer is given the entire quiz text, with that answer (and all
-# previous correct answers) is displayed.
+# unlike madlibs we know exactly what pattern we are looking for to
+# identify a blank space.  There can be multiple occurrences of the same
+# numbered blank.  The only assumptions is that the first occurrence of
+# each numbered blank appear in order (i.e. 1 before 2 and 2
+# before 3).  
 
-MAX_GUESSES = 5
+import os # to get the listdir method
+import re # to use regular expressions for finding blanks
 
-# Quiz texts
-quiz1_text = '''A ___1___ is created with the def keyword. You specify the inputs a 
-___1___ takes by adding ___2___ separated by commas between the parentheses. 
-___1___s by default return ___3___ if you don't specify the value to return. 
-___2___ can be standard data types such as string, number, dictionary, tuple,
-and ___4___ or can be more complicated such as objects and lambda functions.\n\n'''
+MAX_GUESSES = 5             # number of guesses for EACH blank
+quizes_path = "quizes"      # relative path to the quiz text files
+answers_path = "answers"    # relative path to the answer key files
 
-#
-quiz2_text = '''A ___1___ is noice! A ___2___ is better! A ___3___ is the best \n\n'''
+def display_welcome():
+    print ("Welcome to the IPND Stage 2 Quizes.\nYou will be given " +
+    str(MAX_GUESSES) + ''' guesses to correctly identify the missing word that
+    belongs in each blank space (e.g. ___1___).  The answers are not
+    case-sensitive.\n''')
 
-#
-quiz3_text = 'Jack and Jill went up the ___1___'
+# display the available files in the quiz directory
+def get_quiz_list(path):
+    return os.listdir(path)
+    
+# prompt the user for their name and to choose from a list of available quizes
+def get_user_choices(avail_quizes):
+    name = raw_input("Please tell me your name: ")
+    
+    print ('''Choose a quiz by index (i.e. 0, 1, 2 etc.) from the list
+            below: ''')
+    print avail_quizes
+    choice = raw_input("Choice: ")
+    return name, choice
 
-# list of possible quiz texts
-quizes = [quiz1_text, quiz2_text, quiz3_text]
-
-# Placeholders and answers 
-quiz1_blanks  = ["___1___", "___2___", "___3___", "___4___"]
-quiz1_answers = ["function", "arguments", "None", "List"]
-
-quiz2_blanks = ["___1___", "___2___", "___3___"]
-quiz2_answers = ["foo", "bar", "baz"]
-
-quiz3_blanks = ["___1___"]
-quiz3_answers = ["beanstalk"]
+# take a relative path and a file name and return a string containing the text
+# read from the file
+def get_file_text(path, fname):
+    f = open(path + '/' + fname, 'r')
+    text = f.read()
+    return text
 
 # beginning of program
 def take_quiz():
-    # welcome message and prompt for user name
+    # welcome and give the user basic instructions
+    display_welcome()
+   
+    # get the list of available quizes 
+    file_list = get_quiz_list(quizes_path)
+
+    # get the user name and quiz choice
+    user_name, choice = get_user_choices(file_list)
+    print 'You chose quiz ' + choice + '. Good luck, ' + user_name + '!\n'
     
-    # give brief instructions and ask for quiz selection
+    # open the quiz text file and store the text
+    quiz = get_file_text(quizes_path, file_list[int(choice)])
+
+    # open the corresponding answer text file and store as a list
+    # it is assumed that the answer file is named identically to the
+    # quiz file
+    answers = get_file_text(answers_path, file_list[int(choice)])
+    answers = answers.split(',')
+    print quiz    
     
-    choice = raw_input("Choose a quiz: 1, 2, or 3\n\n")
-    
-    print 'You chose quiz ' + choice + '. Good luck!\n\n'
-    quiz = quizes[int(choice) - 1]
-    print quiz
-    
+    # get a list of all blanks that match the pattern
+    matches = re.findall('___.___', quiz)
+    num_unique_matches = len(set(matches))
+    print matches
+
     index = 0 # keep track of which blank / answer pair we are talking about
-    for e in quiz1_blanks:
-        rem_guesses = MAX_GUESSES
-        guesses = 0
-        correct_ans = False
-        while guesses < MAX_GUESSES and not correct_ans:
-            user_input = raw_input("What word goes in: " + e + "?  ")
+    while index < num_unique_matches:
+        remaining_guesses = MAX_GUESSES
+        wrong_guesses = 0
+        correct = False
+        placeholder = matches[index]
+
+        # let the user guess until they either get it right or they use all of
+        # their allotted guesses
+        while wrong_guesses < MAX_GUESSES and not correct:
+            user_input = raw_input("What word goes in: " + placeholder + "?  ")
             #check if the answer given is correct and if so replace all
             #instances of this blank with the answer.
-            answer = quiz1_answers[index]
-            if answer == user_input:
-                correct_ans = True
-                quiz = quiz.replace(e, user_input)
+            correct_answer = answers[index].strip() # strip removes any leading
+            # or trailing whitespace
+            if user_input == correct_answer:
+                correct = True
+                quiz = quiz.replace(placeholder, user_input)
                 print 'Correct!\n'
                 print quiz
                 index += 1
             else:
                 #Otherwise let the user know they got it wrong and report how many
                 #chances they have left
-                guesses += 1
-                rem_guesses -= 1
-                print 'Incorrect! You have ' + str(rem_guesses) + ' guesses remaining.\n\n'
+                wrong_guesses += 1
+                remaining_guesses -= 1
+                print 'Incorrect! You have ' + str(remaining_guesses) + ' guesses remaining.\n\n'
                 print quiz
-        if not correct_ans:
+        if not correct:
             return 'You Lose!\n'
-    if correct_ans:
+    if correct:
         print "You Win!"
         return quiz
     else:
